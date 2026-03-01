dbfc558dcc72da2febe0dd983fb7a106ff98cb76
Cheyxtion <boomrcharg56@gmail.com>
2026-03-02 03:35:41 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Menunggu dokumen selesai dimuat
+document.addEventListener('DOMContentLoaded', () => {
+    const ctaButton = document.getElementById('cta-button');
+
+    ctaButton.addEventListener('click', () => {
+        alert('Halo! Terima kasih sudah mencoba landing page ini. Silakan modifikasi kodenya sesuka hati!');
+    });
+});
